c35ba6732fa8e1ce2e7f10ee8001dfb8a54f30a4
Eric Robert <eric.robert@nubo.ca>
2013-03-10 19:08:42 -0400

    added exercise 2 (Chess-02)

--- CPP023/Chess/Main.cpp
+++ CPP023/Chess/Main.cpp
@@ -1,19 +1,52 @@
 // Copyright (c) 2012 Eric Robert.
 // All rights reserved.
 
+// inclusions
 #include <iostream>
 
-int main() {
-  char * board = " abcdefgh \n"
-                 "8rnbqkbnr8\n"
-                 "7pppppppp7\n"
-                 "6        6\n"
-                 "5        5\n"
-                 "4        4\n"
-                 "3        3\n"
-                 "2PPPPPPPP2\n"
-                 "1RNBQKBNR1\n"
-                 " abcdefgh \n";
+// échiquier 8x8
+char board[] = "RNBQKBNR"
+               "PPPPPPPP"
+               "        "
+               "        "
+               "        "
+               "        "
+               "pppppppp"
+               "rnbqkbnr";
 
-  std::cout << board;
+// affichage de l'échiquier
+void print() {
+  std::cout << " abcdefgh " << std::endl;
+
+  // affiche les pièces noires puis les blanches
+  for(int j = 8; j != 0; --j) {
+    std::cout << j;
+    for(int i = 0; i != 8; ++i) {
+      std::cout << board[i + (j - 1) * 8];
+    }
+
+    // complète la ligne
+    std::cout << j << std::endl;
+  }
+
+  std::cout << " abcdefgh " << std::endl;
+}
+
+// validation des coordonnées
+bool isValid(int i, int j) {
+  return i >= 0 && i < 8 && j >= 0 && j < 8;
+}
+
+// déplacement d'une pièce
+void movePiece(int x, int y, int i, int j) {
+  if(isValid(x, y) && isValid(i, j)) {
+    board[i + j * 8] = board[x + y * 8];
+    board[x + y * 8] = ' ';
+  }
+}
+
+int main() {
+  print();
+  movePiece(1, 0, 2, 2); // bouge le chevalier blanc
+  print();
 }
