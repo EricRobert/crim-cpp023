44af90c35ef17fbe7dbc0b699dc9ae7fb062c889
Eric Robert <eric.robert@nubo.ca>
2013-03-10 19:07:03 -0400

    added exercise 1 (Chess-01)

---
+++ CPP023/Chess/Main.cpp
@@ -0,0 +1,19 @@
+// Copyright (c) 2012 Eric Robert.
+// All rights reserved.
+
+#include <iostream>
+
+int main() {
+  char * board = " abcdefgh \n"
+                 "8rnbqkbnr8\n"
+                 "7pppppppp7\n"
+                 "6        6\n"
+                 "5        5\n"
+                 "4        4\n"
+                 "3        3\n"
+                 "2PPPPPPPP2\n"
+                 "1RNBQKBNR1\n"
+                 " abcdefgh \n";
+
+  std::cout << board;
+}
