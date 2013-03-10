d317fde06c326032dbd255757836c2807c216948
Eric Robert <eric.robert@nubo.ca>
2013-03-10 19:09:56 -0400

    added exercise 3 (Chess-03)

--- CPP023/Chess/Main.cpp
+++ CPP023/Chess/Main.cpp
@@ -4,25 +4,50 @@
 // inclusions
 #include <iostream>
 
-// échiquier 8x8
-char board[] = "RNBQKBNR"
-               "PPPPPPPP"
-               "        "
-               "        "
-               "        "
-               "        "
-               "pppppppp"
-               "rnbqkbnr";
+// validation des coordonnées
+bool isValid(int i, int j) {
+  return i >= 0 && i < 8 && j >= 0 && j < 8;
+}
+
+// représentation de l'échiquier
+class Board
+{
+public:
+  Board();
+
+  void print() const;
+  void movePiece(int x, int y, int i, int j);
+
+private:
+  char squares[64];
+};
+
+// constructeur par défaut
+Board::Board() {
+  char const * board =
+    "RNBQKBNR"
+    "PPPPPPPP"
+    "        "
+    "        "
+    "        "
+    "        "
+    "pppppppp"
+    "rnbqkbnr";
+
+  for(int i = 0; i != 64; ++i) {
+    squares[i] = board[i];
+  }
+}
 
 // affichage de l'échiquier
-void print() {
+void Board::print() const {
   std::cout << " abcdefgh " << std::endl;
 
   // affiche les pièces noires puis les blanches
   for(int j = 8; j != 0; --j) {
     std::cout << j;
     for(int i = 0; i != 8; ++i) {
-      std::cout << board[i + (j - 1) * 8];
+      std::cout << squares[i + (j - 1) * 8];
     }
 
     // complète la ligne
@@ -32,21 +57,24 @@ void print() {
   std::cout << " abcdefgh " << std::endl;
 }
 
-// validation des coordonnées
-bool isValid(int i, int j) {
-  return i >= 0 && i < 8 && j >= 0 && j < 8;
-}
-
 // déplacement d'une pièce
-void movePiece(int x, int y, int i, int j) {
+void Board::movePiece(int x, int y, int i, int j) {
   if(isValid(x, y) && isValid(i, j)) {
-    board[i + j * 8] = board[x + y * 8];
-    board[x + y * 8] = ' ';
+    squares[i + j * 8] = squares[x + y * 8];
+    squares[x + y * 8] = ' ';
   }
 }
 
 int main() {
-  print();
-  movePiece(1, 0, 2, 2); // bouge le chevalier blanc
-  print();
+  // allocation de l'échiquier
+  Board a;
+
+  a.print();
+  a.movePiece(1, 0, 2, 2); // bouge le chevalier blanc
+  a.print();
+
+  // duplique l'échiquier
+  Board b(a);
+  b.movePiece(1, 7, 2, 5); // bouge le chevalier noir
+  b.print();
 }
